6f3918000db8673074a66e565290c5d39bbe7d66
vict890f <vict890f@stud.kea.dk>
2019-09-18 14:29:11 +0200

    .

--- scripthjemme.js
+++ scripthjemme.js
@@ -9,10 +9,12 @@
         //        let filter2015 = "alle";
         //        let filter2014 = "alle";
         //        let filter2013 = "alle";
+
         document.addEventListener("DOMContentLoaded", getJson);
 
 
         async function getJson() {
+
             let jsonData = await fetch("https://spreadsheets.google.com/feeds/list/1KyJ5qZB4RFcKCQNBntAse9_VE26JRWETlt-qz6IogIY/1/public/values?alt=json");
             menuArray = await jsonData.json();
             visKunstnere();
@@ -52,11 +54,95 @@
 
 
 
-
         function addEventListenersToButtons() {
+            document.querySelector("#button8").addEventListener("click", toggleMenu8);
+            document.querySelector("#button7").addEventListener("click", toggleMenu7);
+            document.querySelector("#button6").addEventListener("click", toggleMenu6);
+            document.querySelector("#button5").addEventListener("click", toggleMenu5);
+            document.querySelector("#button4").addEventListener("click", toggleMenu4);
+            document.querySelector("#button3").addEventListener("click", toggleMenu3);
+            document.querySelector("#button2").addEventListener("click", toggleMenu2);
+
             document.querySelectorAll(".filter").forEach(elm => {
                 elm.addEventListener("click", filtrering);
             })
+        }
+
+        function toggleMenu8() {
+            console.log("toggleMenu");
+            document.querySelector("#bg2018").classList.add("display");
+            document.querySelector("#bg2017").classList.add("display");
+            document.querySelector("#bg2016").classList.add("display");
+            document.querySelector("#bg2015").classList.add("display");
+            document.querySelector("#bg2014").classList.add("display");
+            document.querySelector("#bg2013").classList.add("display");
+            document.querySelector("#bg2019").classList.remove("display");
+        }
+
+        function toggleMenu7() {
+            console.log("toggleMenu");
+            document.querySelector("#bg2017").classList.add("display");
+            document.querySelector("#bg2016").classList.add("display");
+            document.querySelector("#bg2015").classList.add("display");
+            document.querySelector("#bg2014").classList.add("display");
+            document.querySelector("#bg2013").classList.add("display");
+            document.querySelector("#bg2019").classList.add("display");
+            document.querySelector("#bg2018").classList.remove("display");
+        }
+
+        function toggleMenu6() {
+            console.log("toggleMenu");
+            document.querySelector("#bg2016").classList.add("display");
+            document.querySelector("#bg2015").classList.add("display");
+            document.querySelector("#bg2014").classList.add("display");
+            document.querySelector("#bg2013").classList.add("display");
+            document.querySelector("#bg2019").classList.add("display");
+            document.querySelector("#bg2018").classList.add("display");
+            document.querySelector("#bg2017").classList.remove("display");
+        }
+
+        function toggleMenu5() {
+            console.log("toggleMenu");
+            document.querySelector("#bg2015").classList.add("display");
+            document.querySelector("#bg2014").classList.add("display");
+            document.querySelector("#bg2013").classList.add("display");
+            document.querySelector("#bg2019").classList.add("display");
+            document.querySelector("#bg2018").classList.add("display");
+            document.querySelector("#bg2017").classList.add("display");
+            document.querySelector("#bg2016").classList.remove("display");
+        }
+
+        function toggleMenu4() {
+            console.log("toggleMenu");
+            document.querySelector("#bg2014").classList.add("display");
+            document.querySelector("#bg2013").classList.add("display");
+            document.querySelector("#bg2019").classList.add("display");
+            document.querySelector("#bg2018").classList.add("display");
+            document.querySelector("#bg2017").classList.add("display");
+            document.querySelector("#bg2016").classList.add("display");
+            document.querySelector("#bg2015").classList.remove("display");
+        }
+
+        function toggleMenu3() {
+            console.log("toggleMenu");
+            document.querySelector("#bg2013").classList.add("display");
+            document.querySelector("#bg2019").classList.add("display");
+            document.querySelector("#bg2018").classList.add("display");
+            document.querySelector("#bg2017").classList.add("display");
+            document.querySelector("#bg2016").classList.add("display");
+            document.querySelector("#bg2015").classList.add("display");
+            document.querySelector("#bg2014").classList.remove("display");
+        }
+
+        function toggleMenu2() {
+            console.log("toggleMenu");
+            document.querySelector("#bg2019").classList.add("display");
+            document.querySelector("#bg2018").classList.add("display");
+            document.querySelector("#bg2017").classList.add("display");
+            document.querySelector("#bg2016").classList.add("display");
+            document.querySelector("#bg2015").classList.add("display");
+            document.querySelector("#bg2014").classList.add("display");
+            document.querySelector("#bg2013").classList.remove("display");
         }
 
         function filtrering() {
