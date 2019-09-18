9b800bd8d513f2b00f7862530e075072e9a99889
vict890f <vict890f@stud.kea.dk>
2019-09-18 14:59:29 +0200

    .

--- scripthjemme.js
+++ scripthjemme.js
@@ -44,7 +44,7 @@
                     dest.appendChild(klon);
 
                     dest.lastElementChild.addEventListener("click", () => {
-                        location.href = `singleview.html?navn=${artist.gsx$navn.$t}`;
+                        visSingle(artist);
                     });
 
                 }
@@ -52,16 +52,39 @@
 
         }
 
+        function visSingle(artist) {
+
+            console.log("vissingleret");
+
+            document.querySelector("#screen_right_json").style.display = "none";
+            document.querySelector("#popup").style.display = "block";
+            document.querySelector("#popup_tilbage").addEventListener("click", lukSingle);
+            document.querySelector("#indhold h2").textContent = artist.gsx$navn.$t;
+            document.querySelector("#indhold h3").textContent = artist.gsx$genre.$t;
+            document.querySelector("#indhold .kort").textContent = artist.gsx$beskrivelse.$t;
+            document.querySelector("#indhold img").src = `assets/forside/${artist.gsx$aargang.$t}/${artist.gsx$ikonid.$t}.jpg`;
+            document.querySelector("#indhold .youtube").src = `https://www.youtube.com/embed/${ artist.gsx$video.$t}`;
+
+
+
+        }
+
+        function lukSingle() {
+            document.querySelector("#popup").style.display = "none";
+            document.querySelector("#screen_right_json").style.display = "block";
+
+        }
+
 
 
         function addEventListenersToButtons() {
-            document.querySelector("#button8").addEventListener("click", toggleMenu8);
-            document.querySelector("#button7").addEventListener("click", toggleMenu7);
-            document.querySelector("#button6").addEventListener("click", toggleMenu6);
+            document.querySelector("#button8").addEventListener("click", toggleMenu2);
+            document.querySelector("#button7").addEventListener("click", toggleMenu3);
+            document.querySelector("#button6").addEventListener("click", toggleMenu4);
             document.querySelector("#button5").addEventListener("click", toggleMenu5);
-            document.querySelector("#button4").addEventListener("click", toggleMenu4);
-            document.querySelector("#button3").addEventListener("click", toggleMenu3);
-            document.querySelector("#button2").addEventListener("click", toggleMenu2);
+            document.querySelector("#button4").addEventListener("click", toggleMenu6);
+            document.querySelector("#button3").addEventListener("click", toggleMenu7);
+            document.querySelector("#button2").addEventListener("click", toggleMenu8);
 
             document.querySelectorAll(".filter").forEach(elm => {
                 elm.addEventListener("click", filtrering);
